e4d522e6245bb61015addbf9a69301b8fbbaefc0
Manoj Mahata <86721538+Manojmahata199@users.noreply.github.com>
2026-02-18 16:42:05 +0530

    update

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ function App() {
           <Route
               path="/add-employee"
               element={
-                  <AddEmployee />
+                <AddEmployee />
               }
             />
 
