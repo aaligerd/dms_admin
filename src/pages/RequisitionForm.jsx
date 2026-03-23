3613c85e2c4752c8b64b04485ffaf768f3245c01
Manoj Mahata <86721538+Manojmahata199@users.noreply.github.com>
2026-03-23 18:22:17 +0530

    Update RequisitionForm.jsx

--- src/pages/RequisitionForm.jsx
+++ src/pages/RequisitionForm.jsx
@@ -121,57 +121,14 @@ const RequisitionForm = () => {
 
             <form onSubmit={handleSubmit} className="p-8">
 
+
+              <div className="grid grid-cols-1 md:grid-cols-1 my-2">
+                <label className="label-2">1. Requisition Details</label>
+              </div>
+
               <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
 
-                {/* ✅ POSITION DROPDOWN */}
-                <div>
-                  <label className="label">Position</label>
-                  <input
-                    type="text"
-                    name="req_pos_title"
-                    value={formData.req_pos_title || ""}
-                    onChange={handleChange}
-                    className="input"
-                    required
-                  />
-
-                  {/* <select
-                    name="req_pos_title"
-                    value={formData.req_pos_title || ""}
-                    onChange={handleChange}
-                    className="input"
-                    required
-                  >
-                    <option value="">Select Position</option>
-                    {positions.map((pos) => (
-                      <option
-                        key={pos.position_id || pos.id}
-                        value={pos.position_id || pos.id}
-                      >
-                        {pos.position_name || pos.name}
-                      </option>
-                    ))}
-                  </select> */}
-                </div>
-
-                {/* Organisation */}
-                <div>
-                  <label className="label">Organisation</label>
-                  <select
-                    name="req_org"
-                    value={formData.req_org || ""}
-                    onChange={handleChange}
-                    className="input"
-                    required
-                  >
-                    <option value="">Select Organisation</option>
-                    {organisations.map((org) => (
-                      <option key={org.org_id} value={org.org_id}>
-                        {org.org_name}
-                      </option>
-                    ))}
-                  </select>
-                </div>
+                
 
                 {/* Department */}
                 <div>
@@ -205,9 +162,42 @@ const RequisitionForm = () => {
                   />
                 </div>
 
+
+                {/* Approval Authority */}
+                <div>
+                  <label className="label">Approval Authority</label>
+                  <input
+                    type="text"
+                    name="aproval_authority"
+                    value={formData.aproval_authority || ""}
+                    onChange={handleChange}
+                    className="input"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="grid grid-cols-1 md:grid-cols-1 my-2">
+                <label className="label-2">2. Position Details</label>
+              </div>
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+
+                {/* ✅ POSITION */}
+                <div>
+                  <label className="label">Job Title or Position</label>
+                  <input
+                    type="text"
+                    name="req_pos_title"
+                    value={formData.req_pos_title || ""}
+                    onChange={handleChange}
+                    className="input"
+                    required
+                  />
+
+                </div>
+
                 {/* Persons Needed */}
                 <div>
-                  <label className="label">Number of Persons</label>
+                  <label className="label">Number of Positions Required</label>
                   <input
                     type="number"
                     min="1"
@@ -219,43 +209,240 @@ const RequisitionForm = () => {
                   />
                 </div>
 
+
+                {/* Employee Type - Permanent / Contractual / Consultant / Freelancer / Contributor / News Agency / Internship / FE / FB*/}
+                <div>
+                  <label className="label">Employee Type</label>
+                  <select
+                    name="emp_type"
+                    value={formData.emp_type || ""}
+                    onChange={handleChange}
+                    className="input"
+                    required
+                  >
+                    <option value="">Select Employee Type</option>
+                    <option value="Permanent">Permanent</option>
+                    <option value="Contractual">Contractual</option>
+                    <option value="Consultant">Consultant</option>
+                    <option value="Freelancer">Freelancer</option>
+                    <option value="Contributor">Contributor</option>
+                    <option value="News Agency">News Agency</option>
+                    <option value="Internship">Internship</option>
+                    <option value="FE">FE</option>
+                    <option value="FB">FB</option>
+                  
+                  </select>
+                </div>
+
+                {/* Organisation */}
+                {/* <div>
+                  <label className="label">Organisation</label>
+                  <select
+                    name="req_org"
+                    value={formData.req_org || ""}
+                    onChange={handleChange}
+                    className="input"
+                    required
+                  >
+                    <option value="">Select Organisation</option>
+                    {organisations.map((org) => (
+                      <option key={org.org_id} value={org.org_id}>
+                        {org.org_name}
+                      </option>
+                    ))}
+                  </select>
+                </div> */}
+
+                
+
+
+                {/* ✅ Location */}
+                <div>
+                  <label className="label">Location</label>
+                  <input
+                    type="text"
+                    name="req_location"
+                    value={formData.req_location || ""}
+                    onChange={handleChange}
+                    className="input"
+                    required
+                  />
+
+                </div>
+
+
+                {/* ✅ Reporting Manager: */}
+                <div>
+                  <label className="label">Reporting Manager</label>
+                  <input
+                    type="text"
+                    name="reporting_manager"
+                    value={formData.reporting_manager || ""}
+                    onChange={handleChange}
+                    className="input"
+                    required
+                  />
+
+                </div>
+
+              </div>
+              <div className="grid grid-cols-1 md:grid-cols-1 my-2">
+                <label className="label-2">3. Justification for Recruitment</label>
+              </div>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              
+
+                {/* Reason */}
+                <div className="mt-6">
+                  <label className="label">Reason for Hiring</label>
+                  <textarea
+                    name="req_reason"
+                    value={formData.req_reason || ""}
+                    onChange={handleChange}
+                    rows="3"
+                    className="input"
+                    required
+                  />
+                </div>
+
+                {/* Brief */}
+                <div className="mt-6">
+                  <label className="label">Brief Justification</label>
+                  <textarea
+                    name="req_brif"
+                    value={formData.req_brif || ""}
+                    onChange={handleChange}
+                    rows="3"
+                    className="input"
+                    required
+                  />
+                </div>   
               </div>
 
-              {/* Reason */}
-              <div className="mt-6">
-                <label className="label">Reason</label>
-                <textarea
-                  name="req_reason"
-                  value={formData.req_reason || ""}
-                  onChange={handleChange}
-                  rows="3"
-                  className="input"
-                  required
-                />
+              <div className="grid grid-cols-1 md:grid-cols-1 my-2">
+                <label className="label-2">4. Job Details</label>
+              </div>
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {/* ✅ Job Description Attached: */}
+                <div>
+                  <label className="label">Job Description Attached</label>
+                  <select
+                    name="job_desc_attachment"
+                    value={formData.job_desc_attachment || ""}
+                    onChange={handleChange}
+                    className="input"
+                    required
+                  >
+                    <option value="Yes">Yes</option>
+                    <option value="No">No</option>
+                  
+                  </select>
+
+                </div>
+                {/* ✅  Key Skills Required: */}
+                <div>
+                  <label className="label">Key Skills Required</label>
+                  <input
+                    type="text"
+                    name="skills_req"
+                    value={formData.skills_req || ""}
+                    onChange={handleChange}
+                    className="input"
+                    required
+                  />
+
+                </div>
+                {/* ✅ Experience Level:: */}
+                <div>
+                  <label className="label">Experience Level</label>
+                  <input
+                    type="text"
+                    name="exp_level"
+                    value={formData.exp_level || ""}
+                    onChange={handleChange}
+                    className="input"
+                    required
+                  />
+
+                </div>
+                {/* ✅ Educational Qualifications: */}
+                <div>
+                  <label className="label">Educational Qualifications</label>
+                  <input
+                    type="text"
+                    name="edu_qualification"
+                    value={formData.edu_qualification || ""}
+                    onChange={handleChange}
+                    className="input"
+                    required
+                  />
+
+                </div>
               </div>
 
-              {/* Brief */}
-              <div className="mt-6">
-                <label className="label">Brief Description</label>
-                <textarea
-                  name="req_brif"
-                  value={formData.req_brif || ""}
-                  onChange={handleChange}
-                  rows="4"
-                  className="input"
-                  required
-                />
+              <div className="grid grid-cols-1 md:grid-cols-1 my-2">
+                <label className="label-2">5. Compensation and Budget </label>
+              </div>
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {/* ✅ Salary Range (CTC): */}
+                <div>
+                  <label className="label">Salary Range (CTC):</label>
+                  <input
+                    type="text"
+                    name="sallery_range"
+                    value={formData.sallery_range || ""}
+                    onChange={handleChange}
+                    className="input"
+                    required
+                  />
+
+                </div>
+                {/* ✅ Budget Approval: */}
+                <div>
+                  <label className="label">Budget Approval</label>
+                  <select
+                    name="budget_approval"
+                    value={formData.budget_approval || ""}
+                    onChange={handleChange}
+                    className="input"
+                    required
+                  >
+                    <option value="Approved">Approved</option>
+                    <option value="Pending">Pending</option>
+                  
+                  </select>
+
+                </div>
+                
+              </div>
+              <div className="grid grid-cols-1 md:grid-cols-1 my-2">
+                <label className="label-2">6. Approval and Signatures</label>
+              </div>
+              <div className="grid grid-cols-1 md:grid-cols-1 gap-6">
+                {/* ✅ Salary Range (CTC): */}
+                <div>
+                  <label className="label">Approver (Add Approver):</label>
+                  <input
+                    type="text"
+                    name="approver"
+                    value={formData.approver || ""}
+                    onChange={handleChange}
+                    className="input"
+                    required
+                  />
+
+                </div>
               </div>
 
               {/* Submit */}
-              <div className="mt-8 flex justify-end">
-                <button
-                  type="submit"
-                  className="px-6 py-2 bg-indigo-600 hover:bg-indigo-500 text-white rounded-md shadow-sm transition"
-                >
-                  Submit Requisition
-                </button>
-              </div>
+                <div className="mt-8 flex justify-end">
+                  <button
+                    type="submit"
+                    className="px-6 py-2 bg-indigo-600 hover:bg-indigo-500 text-white rounded-md shadow-sm transition"
+                  >
+                    Submit Requisition
+                  </button>
+                </div>
 
             </form>
           </div>
@@ -268,6 +455,14 @@ const RequisitionForm = () => {
           display:block;
           font-size:14px;
           color:#4b5563;
+          margin-bottom:6px;
+          font-weight:500;
+          text-align:left;
+        }
+          .label-2 {
+          display:block;
+          font-size:16px;
+          color:#000;
           margin-bottom:6px;
           font-weight:500;
           text-align:left;
